fb6f0b7aa9b45a1c00c50b596271ea635a88f8bd
Kevin Today <1142185+mieubrisse@users.noreply.github.com>
2026-01-31 06:31:47 -0300

    Add template install command for cloning GitHub repos

---
+++ cmd/template_install.go
@@ -0,0 +1,54 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/mieubrisse/stacktrace"
+	"github.com/spf13/cobra"
+
+	"github.com/odyssey/agenc/internal/config"
+)
+
+var templateInstallCmd = &cobra.Command{
+	Use:   "install owner/repo-name",
+	Short: "Install an agent template from a GitHub repository",
+	Args:  cobra.ExactArgs(1),
+	RunE:  runTemplateInstall,
+}
+
+func init() {
+	templateCmd.AddCommand(templateInstallCmd)
+}
+
+func runTemplateInstall(cmd *cobra.Command, args []string) error {
+	ownerRepo := args[0]
+
+	parts := strings.SplitN(ownerRepo, "/", 3)
+	if len(parts) != 2 || parts[0] == "" || parts[1] == "" {
+		return stacktrace.NewError("argument must be in the format 'owner/repo-name'")
+	}
+	repoName := parts[1]
+
+	templatesDirpath := config.GetAgentTemplatesDirpath(agencDirpath)
+	targetDirpath := config.GetAgentTemplateDirpath(agencDirpath, repoName)
+
+	if _, err := os.Stat(targetDirpath); err == nil {
+		return stacktrace.NewError("template '%s' already exists at %s", repoName, targetDirpath)
+	}
+
+	cloneURL := fmt.Sprintf("https://github.com/%s.git", ownerRepo)
+	gitCmd := exec.Command("git", "clone", cloneURL, targetDirpath)
+	gitCmd.Dir = templatesDirpath
+	gitCmd.Stdout = os.Stdout
+	gitCmd.Stderr = os.Stderr
+
+	if err := gitCmd.Run(); err != nil {
+		return stacktrace.Propagate(err, "failed to clone repository '%s'", ownerRepo)
+	}
+
+	fmt.Printf("Installed template '%s' from %s\n", repoName, cloneURL)
+	return nil
+}
