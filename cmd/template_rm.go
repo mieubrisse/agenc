5a2baa22e083ef8990e3c1496dd3ded975f1c388
Kevin Today <1142185+mieubrisse@users.noreply.github.com>
2026-02-02 11:24:33 -0300

    Add template rm command

---
+++ cmd/template_rm.go
@@ -0,0 +1,56 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mieubrisse/stacktrace"
+	"github.com/spf13/cobra"
+
+	"github.com/odyssey/agenc/internal/config"
+)
+
+var templateRmCmd = &cobra.Command{
+	Use:   "rm <template>",
+	Short: "Remove an installed agent template",
+	Args:  cobra.ExactArgs(1),
+	RunE:  runTemplateRm,
+}
+
+func init() {
+	templateCmd.AddCommand(templateRmCmd)
+}
+
+func runTemplateRm(cmd *cobra.Command, args []string) error {
+	cfg, err := config.ReadAgencConfig(agencDirpath)
+	if err != nil {
+		return stacktrace.Propagate(err, "failed to read config")
+	}
+
+	repo, err := resolveTemplate(cfg.AgentTemplates, args[0])
+	if err != nil {
+		return err
+	}
+
+	// Remove from config
+	filtered := cfg.AgentTemplates[:0]
+	for _, entry := range cfg.AgentTemplates {
+		if entry.Repo != repo {
+			filtered = append(filtered, entry)
+		}
+	}
+	cfg.AgentTemplates = filtered
+
+	if err := config.WriteAgencConfig(agencDirpath, cfg); err != nil {
+		return stacktrace.Propagate(err, "failed to write config")
+	}
+
+	// Remove cloned repo from disk
+	repoDirpath := config.GetRepoDirpath(agencDirpath, repo)
+	if err := os.RemoveAll(repoDirpath); err != nil {
+		return stacktrace.Propagate(err, "failed to remove repo directory '%s'", repoDirpath)
+	}
+
+	fmt.Printf("Removed template '%s'\n", repo)
+	return nil
+}
