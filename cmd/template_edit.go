514aae0f26838191be22671a85b38b641ffd6d39
Kevin Today <1142185+mieubrisse@users.noreply.github.com>
2026-02-04 19:49:26 -0300

    Fix template edit to use defaultFor:agentTemplate when resolving agent template

--- cmd/template_edit.go
+++ cmd/template_edit.go
@@ -38,6 +38,11 @@ func runTemplateEdit(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
+	agentTemplate, err := resolveAgentTemplate(cfg, "", templateName)
+	if err != nil {
+		return err
+	}
+
 	templateCloneDirpath := config.GetRepoDirpath(agencDirpath, templateName)
-	return createAndLaunchMission(agencDirpath, "", templateName, templateCloneDirpath)
+	return createAndLaunchMission(agencDirpath, agentTemplate, templateName, templateCloneDirpath)
 }
